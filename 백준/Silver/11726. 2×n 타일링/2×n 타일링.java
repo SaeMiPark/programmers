3c7536c6af21ba0321cd1b679f8e40336478907d
Park Sae Mi <sami3013@naver.com>
2024-03-24 15:59:33 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -10,6 +10,7 @@ public class Main {
         int[] dp = new int[n+1];
         dp[0]=1;
         dp[1]=1;
+//        dp[2]=2하면 n=1일때 ArrayIndexOut 에러가 발생
         for(int i=2; i<=n; i++){
             dp[i]=(dp[i-1]+dp[i-2])%10007;
         }
